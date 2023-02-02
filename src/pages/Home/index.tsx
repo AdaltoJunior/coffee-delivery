6973debf2a9cd7869d012ec2f2b7b405fad74010
Adalto Junior <mendesrochajunior@gmail.com>
2023-02-01 22:04:40 -0300

    refactor: remove filter from home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,6 @@
 import { useCoffees } from '../../hooks/useCoffees'
 
 import { Hero } from '../../components/Hero'
-import { Filter } from '../../components/Filter'
 import { Heading } from '../../components/Heading'
 import { Container } from '../../components/Container'
 import { CoffeeCard } from '../../components/CoffeeCard'
@@ -9,7 +8,7 @@ import { CoffeeCard } from '../../components/CoffeeCard'
 import { CoffeeList, CoffeeSection, CoffeeSectionHeader } from './styles'
 
 export function Home() {
-  const { coffees, categories } = useCoffees()
+  const { coffees } = useCoffees()
 
   return (
     <main>
@@ -19,7 +18,6 @@ export function Home() {
         <Container>
           <CoffeeSectionHeader>
             <Heading fontWeight="extraBold">Nossos cafés</Heading>
-            <Filter data={categories} />
           </CoffeeSectionHeader>
 
           <CoffeeList>
